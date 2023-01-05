0dbbcbe77576ce9245f1af9b60ea358c8a604f2b
Timothy Njoroge <timothynjoroge56@gmail.com>
2023-01-05 21:43:03 +0300

    Dynamically change footer logo with javascript functionality

--- js/about.js
+++ js/about.js
@@ -3,9 +3,19 @@ const closeButton = document.querySelector('.close');
 const nav = document.querySelector('.nav__wrapper');
 
 menuButton.addEventListener('click', () => {
-    nav.classList.toggle('open');
+  nav.classList.toggle('open');
 });
 
 closeButton.addEventListener('click', () => {
-    nav.classList.toggle('open');
+  nav.classList.toggle('open');
+});
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 768) {
+    const footerImg = document.querySelector('.ending .img__wrapper img');
+    footerImg.src = '../assets/images/icons/cck-logo-about.png';
+  } else {
+    const footerImg = document.querySelector('.ending .img__wrapper img');
+    footerImg.src = '../assets/images/icons/cck-logo.png';
+  }
 });
